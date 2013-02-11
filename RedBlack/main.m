cb87f40feed3b95010df51eca3343d936fb9c25a
Prachi Gauriar <prachi@quantumlenscap.com>
2013-02-10 23:12:02 -0500

    Initial Commit

---
+++ RedBlack/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RedBlack
+//
+//  Created by Prachi Gauriar on 2/10/2013.
+//  Copyright (c) 2013 Prachi Gauriar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
